870184b57be98f54882f9352e39d077d0b9ce873
jydxh <wzxxds@gmail.com>
2023-10-29 13:58:26 -0400

    1按钮组件

---
+++ ele_project/src/App.vue
@@ -0,0 +1,47 @@
+<template>
+	<div id="app">
+		<nav>
+			<router-link to="/">Home</router-link> |
+			<router-link to="/about">About</router-link>
+		</nav>
+		<el-row>
+			<el-button>默认按钮</el-button>
+			<el-button type="primary">主要按钮</el-button>
+			<el-button type="success">成功按钮</el-button>
+			<el-button type="info">信息按钮</el-button>
+			<el-button type="warning">警告按钮</el-button>
+			<el-button type="danger">危险按钮</el-button>
+		</el-row>
+		<el-row>
+			<el-button type="primary" icon="el-icon-edit"></el-button>
+			<el-button type="primary" icon="el-icon-share"></el-button>
+			<el-button type="primary" icon="el-icon-delete"></el-button>
+			<el-button type="primary" icon="el-icon-search">搜索</el-button>
+			<el-button type="primary">上传<i class="el-icon-upload el-icon--right"></i></el-button>
+		</el-row>
+		<router-view />
+	</div>
+</template>
+
+<style lang="scss">
+	#app {
+		font-family: Avenir, Helvetica, Arial, sans-serif;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+		text-align: center;
+		color: #2c3e50;
+	}
+
+	nav {
+		padding: 30px;
+
+		a {
+			font-weight: bold;
+			color: #2c3e50;
+
+			&.router-link-exact-active {
+				color: #42b983;
+			}
+		}
+	}
+</style>
