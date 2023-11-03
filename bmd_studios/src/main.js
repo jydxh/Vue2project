d1827fc211ae23a4acd6cb57c3e6824cc6f01302
jydxh <wzxxds@gmail.com>
2023-11-03 11:35:20 -0400

    封装axios使用Vue.prototype全局变量保存数据

--- bmd_studios/src/main.js
+++ bmd_studios/src/main.js
@@ -1,17 +1,21 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
 
 // 引入ElementUI
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
+import ElementUI from "element-ui";
+import "element-ui/lib/theme-chalk/index.css";
 Vue.use(ElementUI);
+/* 为Vue对象注入自定义属性 全局变量 */
+import BaseUrl from "./http/BaseUrl";
+const uploadURL = BaseUrl.UPLOADURL;
+Vue.prototype.uploadURL = uploadURL;
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+	router,
+	store,
+	render: h => h(App),
+}).$mount("#app");
--- bmd_studios/src/views/actor/ActorAdd.vue
+++ bmd_studios/src/views/actor/ActorAdd.vue
@@ -15,7 +15,7 @@
 			<el-form-item label="头像" prop="actorAvatar">
 				<el-upload
 					class="avatar-uploader"
-					:action="url + '/upload'"
+					:action="uploadURL + '/upload'"
 					:show-file-list="false"
 					:on-success="handleAvatarSuccess"
 					:before-upload="beforeAvatarUpload">
@@ -34,12 +34,12 @@
 <script>
 	import myaxios from "@/http/Myaxios.js";
 	import httpApi from "@/http/index.js";
-	import BaseUrl from "../../http/BaseUrl.js";
-	const url = BaseUrl.UPLOADURL;
+	/* import BaseUrl from "../../http/BaseUrl.js";
+	const url = BaseUrl.UPLOADURL; */
 	export default {
 		data() {
 			return {
-				url,
+				// url,
 				form: {
 					actorName: "", // 封装演员名字
 					actorAvatar: "", // 封装演员头像路径
@@ -50,9 +50,7 @@
 				},
 			};
 		},
-		mounted() {
-			console.log(url);
-		},
+
 		methods: {
 			/* 取消重置表单 */
 			cancel() {
